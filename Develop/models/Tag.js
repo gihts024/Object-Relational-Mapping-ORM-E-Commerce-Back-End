430882191980bf660d7b8d86675cc750d7ebe1a9
Alex Gihts <githenyalex@gmail.com>
2021-08-12 11:01:55 -0700

    started project

---
+++ Develop/models/Tag.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: Product,
+            key: id,
+          }
+
+        },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: id,
+      }
+
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,96 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // be sure to include its associated Products
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // be sure to include its associated Products
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  try {
+    const locationData = await Category.create({
+      product_id: req.body.product_id,
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+    //Calls the update method on the Book model
+    Product.update(
+      {
+        // All the fields you can update and the data attached to the request body.
+        id: req.body.id,
+      
+      },
+      {
+        // Gets a Product based on the Product_id given in the request parameters
+        where: {
+          category_id: req.params.category_id,
+        },
+      }
+    )
+      .then((updatedProduct) => {
+        res.json(updatedProduct);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
